355ee687402402bfa627f2108a8e7d22add2c885
Chhaileng Peng <chhailengpeng@gmail.com>
2020-07-27 16:49:11 +0900

    Update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -3,7 +3,7 @@ wget https://golang.org/dl/go1.14.6.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.14.6.linux-amd64.tar.gz
 
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile && \
-echo 'export GOPATH=$HOME/gopath' | tee -a $HOME/.profile && \
-echo 'export GOROOT=$HOME/go' | tee -a $HOME/.profile && \
-echo 'export PATH=$PATH:$GOROOT/bin' | tee -a $HOME/.profile && \
+echo 'export GOPATH=$HOME/go' | tee -a $HOME/.profile && \
+echo 'export GOROOT=/usr/local/go' | tee -a $HOME/.profile && \
+echo 'export PATH=$PATH:$GOPATH/bin' | tee -a $HOME/.profile && \
 mkdir -p $HOME/go/{src,pkg,bin}
